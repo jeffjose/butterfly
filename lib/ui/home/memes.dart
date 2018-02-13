f0a72a7a12c5ddba33b46ebcb8b4070087175bbb
Jeffrey Jose <jeffjosejeff@gmail.com>
2018-02-13 15:18:01 -0800

    getting code from flutter/plugins for video

--- lib/ui/home/memes.dart
+++ lib/ui/home/memes.dart
@@ -4,8 +4,246 @@ import 'package:butterfly/routes.dart';
 import 'package:butterfly/theme.dart' as Theme;
 import 'package:butterfly/model/memedata.dart';
 import 'package:flutter_image/network.dart';
-//import 'package:flutter_animated/animated_gif.dart';
 import 'package:video_player/video_player.dart';
+
+
+class AspectRatioVideo extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  AspectRatioVideo(this.controller);
+
+  @override
+  AspectRatioVideoState createState() => new AspectRatioVideoState();
+}
+
+/// Controls play and pause of [controller].
+///
+/// Toggles play/pause on tap (accompanied by a fading status icon).
+///
+/// Plays (looping) on initialization, and mutes on deactivation.
+class VideoPlayPause extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  VideoPlayPause(this.controller);
+
+  @override
+  State createState() {
+    return new _VideoPlayPauseState();
+  }
+}
+
+class _VideoPlayPauseState extends State<VideoPlayPause> {
+  FadeAnimation imageFadeAnim =
+      new FadeAnimation(child: new Icon(Icons.play_arrow, size: 100.0));
+  VoidCallback listener;
+
+  _VideoPlayPauseState() {
+    listener = () {
+      setState(() {});
+    };
+  }
+
+  VideoPlayerController get controller => widget.controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(listener);
+    controller.setVolume(1.0);
+    controller.play();
+  }
+
+  @override
+  void deactivate() {
+    controller.setVolume(0.0);
+    controller.removeListener(listener);
+    super.deactivate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> children = <Widget>[
+      new GestureDetector(
+        child: new VideoPlayer(controller),
+        onTap: () {
+          if (!controller.value.initialized) {
+            return;
+          }
+          if (controller.value.isPlaying) {
+            imageFadeAnim =
+                new FadeAnimation(child: new Icon(Icons.pause, size: 100.0));
+            controller.pause();
+          } else {
+            imageFadeAnim = new FadeAnimation(
+                child: new Icon(Icons.play_arrow, size: 100.0));
+            controller.play();
+          }
+        },
+      ),
+      new Align(
+        alignment: Alignment.bottomCenter,
+        child: new VideoProgressIndicator(
+          controller,
+          allowScrubbing: true,
+        ),
+      ),
+      new Center(child: imageFadeAnim),
+    ];
+
+    return new Stack(
+      fit: StackFit.passthrough,
+      children: children,
+    );
+  }
+}
+
+class FadeAnimation extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+
+  FadeAnimation({this.child, this.duration: const Duration(milliseconds: 500)});
+
+  @override
+  _FadeAnimationState createState() => new _FadeAnimationState();
+}
+
+class _FadeAnimationState extends State<FadeAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        new AnimationController(duration: widget.duration, vsync: this);
+    animationController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+    animationController.forward(from: 0.0);
+  }
+
+  @override
+  void deactivate() {
+    animationController.stop();
+    super.deactivate();
+  }
+
+  @override
+  void didUpdateWidget(FadeAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.child != widget.child) {
+      animationController.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return animationController.isAnimating
+        ? new Opacity(
+            opacity: 1.0 - animationController.value,
+            child: widget.child,
+          )
+        : new Container();
+  }
+}
+
+typedef Widget VideoWidgetBuilder(
+    BuildContext context, VideoPlayerController controller);
+
+/// A widget connecting its life cycle to a [VideoPlayerController].
+class PlayerLifeCycle extends StatefulWidget {
+  final VideoWidgetBuilder childBuilder;
+  final String uri;
+
+  PlayerLifeCycle(this.uri, this.childBuilder);
+
+  @override
+  _PlayerLifeCycleState createState() => new _PlayerLifeCycleState();
+}
+
+class _PlayerLifeCycleState extends State<PlayerLifeCycle> {
+  VideoPlayerController controller;
+
+  _PlayerLifeCycleState();
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new VideoPlayerController(widget.uri);
+    controller.addListener(() {
+      if (controller.value.hasError) {
+        print(controller.value.errorDescription);
+      }
+    });
+    controller.initialize();
+    controller.setLooping(true);
+    controller.play();
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.childBuilder(context, controller);
+  }
+}
+
+class AspectRatioVideoState extends State<AspectRatioVideo> {
+  VideoPlayerController get controller => widget.controller;
+  bool initialized = false;
+
+  VoidCallback listener;
+
+  @override
+  void initState() {
+    super.initState();
+    listener = () {
+      if (!mounted) {
+        return;
+      }
+      if (initialized != controller.value.initialized) {
+        initialized = controller.value.initialized;
+        setState(() {});
+      }
+    };
+    controller.addListener(listener);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (initialized) {
+      final Size size = controller.value.size;
+      return new Center(
+        child: new AspectRatio(
+          aspectRatio: size.width / size.height,
+          child: new VideoPlayPause(controller),
+        ),
+      );
+    } else {
+      return new Container();
+    }
+  }
+}
+
+
+
 
 class Media extends StatelessWidget {
 
@@ -18,20 +256,41 @@ class Media extends StatelessWidget {
 
     if (this.url.endsWith('.webm')) {
 
-      return new Image.network('https://www.webmproject.org/media/images/webm-558x156.png',
-          height: 400.0,
-          fit: BoxFit.fitWidth,
-          gaplessPlayback: true,
-          );
+      return new PlayerLifeCycle(this.url,
+          (BuildContext context, VideoPlayerController controller) => new AspectRatioVideo(controller));
+      //final VideoPlayerController controller = new VideoPlayerController(this.url,);
+      //controller.setLooping(true);
+      //controller.play();
+      //controller.setVolume(0.0);
+      //controller.initialize();
+
+      //return new VideoPlayer(controller);
+
+      //return new Image.network('https://www.webmproject.org/media/images/webm-558x156.png',
+      //    height: 400.0,
+      //    fit: BoxFit.fitWidth,
+      //    gaplessPlayback: true,
+      //    );
     }
 
     else if (this.url.endsWith('.mp4')) {
 
-      return new Image.network('https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/112012/mp4_video.jpg',
-          height: 400.0,
-          fit: BoxFit.fitWidth,
-          gaplessPlayback: true,
-          );
+      return new PlayerLifeCycle(this.url,
+          (BuildContext context, VideoPlayerController controller) => new AspectRatioVideo(controller));
+
+      //final VideoPlayerController controller = new VideoPlayerController(this.url,);
+      //controller.setLooping(true);
+      //controller.play();
+      //controller.setVolume(0.0);
+      //controller.initialize();
+
+      //return new VideoPlayer(controller);
+
+      //return new Image.network('https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/112012/mp4_video.jpg',
+      //    height: 400.0,
+      //    fit: BoxFit.fitWidth,
+      //    gaplessPlayback: true,
+      //    );
     }
     else {
       return new Image.network(this.url,
