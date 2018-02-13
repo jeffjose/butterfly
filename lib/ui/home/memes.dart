c7399a128d1d16d4343cf9d14b1c1846c46ec412
Jeffrey Jose <jeffjosejeff@gmail.com>
2018-02-12 22:20:58 -0800

    Adding code to fork for video and image, and setup apk release

--- lib/ui/home/memes.dart
+++ lib/ui/home/memes.dart
@@ -5,6 +5,45 @@ import 'package:butterfly/theme.dart' as Theme;
 import 'package:butterfly/model/memedata.dart';
 import 'package:flutter_image/network.dart';
 //import 'package:flutter_animated/animated_gif.dart';
+import 'package:video_player/video_player.dart';
+
+class Media extends StatelessWidget {
+
+  final String url;
+
+  const Media({Key key, this.url}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    if (this.url.endsWith('.webm')) {
+
+      return new Image.network('https://www.webmproject.org/media/images/webm-558x156.png',
+          height: 400.0,
+          fit: BoxFit.fitWidth,
+          gaplessPlayback: true,
+          );
+    }
+
+    else if (this.url.endsWith('.mp4')) {
+
+      return new Image.network('https://botw-pd.s3.amazonaws.com/styles/logo-thumbnail/s3/112012/mp4_video.jpg',
+          height: 400.0,
+          fit: BoxFit.fitWidth,
+          gaplessPlayback: true,
+          );
+    }
+    else {
+      return new Image.network(this.url,
+          height: 400.0,
+          fit: BoxFit.fitWidth,
+          gaplessPlayback: true,
+          );
+    }
+
+  }
+
+}
 
 class MemeItem extends StatelessWidget {
   final Meme meme;
@@ -18,12 +57,17 @@ class MemeItem extends StatelessWidget {
       new Icon(Icons.comment),
     ]);
 
-    final image = new Image(
-      fit: BoxFit.fitHeight,
-      image: new NetworkImageWithRetry(meme.url),
-    );
+    //final image = new Image.network(
+    //    meme.url,
+    //    height: 400.0,
+    //    fit: BoxFit.fitWidth,
+    //    gaplessPlayback: true
+    //    );
 
-    return new Column(children: <Widget>[image, info]);
+
+    final media = new Media(url: meme.url);
+
+    return new Column(children: <Widget>[media, info]);
   }
 }
 
